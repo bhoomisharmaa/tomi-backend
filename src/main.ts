3efff58ddeefcef646b6e51fea329386726c8576
bhoomisharmaa <bhoomisharma404@gmail.com>
2024-07-12 02:10:06 +0530

    backend

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 3001;
+  await app.listen(PORT);
+  console.log('App is listening on port', PORT);
+}
+bootstrap();
